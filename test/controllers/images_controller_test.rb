42d771d7b66d3bd00a3b7e5287220eb135e8da58
Ilya <simonewarlet@gmail.com>
2024-12-05 14:53:21 +0200

    Continue adding image entity

---
+++ test/controllers/images_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class ImagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get images_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get images_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get images_destroy_url
+    assert_response :success
+  end
+end
