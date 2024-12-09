7ba672ad84ddc9b332e3e0d39795db1ca556340c
Ilya <simonewarlet@gmail.com>
2024-12-09 15:35:31 +0200

    Add sorting by like to images inside category

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@ class Category < ApplicationRecord
   friendly_id :title, use: :slugged
 
   belongs_to :user
-  has_many :images, dependent: :destroy
+  has_many :images, -> { order(likes_count: :desc) }, dependent: :destroy
   # has_many :subscriptions, dependent: :destroy
   accepts_nested_attributes_for :images
   validates :title, presence: true, uniqueness: true
