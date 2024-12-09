dc994260c25b21816084b1dcea75c30e6af6a3f1
Ilya <simonewarlet@gmail.com>
2024-12-09 16:04:53 +0200

    Add top five categories to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,12 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, only: [ :users ], unless: :admin_signed_in?
   before_action :authendicate_admin, only: [ :admin ]
   def home
+    @top_categories = Category
+      .select("categories.*, COUNT(likes.id) AS likes_count")
+      .joins(images: :likes)
+      .group("categories.id")
+      .order("likes_count DESC")
+      .limit(5)
   end
   def users
   end
