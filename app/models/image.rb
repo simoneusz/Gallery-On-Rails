b37b858859bddf11b7f9bd8ac2e57d21e80e0efe
Ilya <simonewarlet@gmail.com>
2024-12-09 11:49:19 +0200

    Add default values for likes, comments to image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,14 @@
 class Image < ApplicationRecord
+  before_save :default_values
+
   belongs_to :category
   belongs_to :user
   mount_uploader :image, ImageUploader
 
   validates :title, presence: true
+
+  def default_values
+    self.likes_count ||= 0
+    self.comments_count ||= 0
+  end
 end
