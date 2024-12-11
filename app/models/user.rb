73747ba84bcfe14eeda6746737243f1389d81c01
Ilya <simonewarlet@gmail.com>
2024-12-11 14:50:26 +0200

    Add avatar to user using avatar uploader with carrierwave

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
+  mount_uploader :avatar, AvatarUploader
 end
---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,29 @@
+class AvatarUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_allowlist
+    %w[jpg jpeg png]
+  end
+
+  version :thumbnail do
+    process resize_to_fill: [ 50, 50 ]
+  end
+
+  version :medium do
+    process resize_to_fill: [ 200, 200 ]
+  end
+
+  def size_range
+    1.byte..5.megabytes
+  end
+
+  def default_url(*args)
+    "/images/fallback/" + [ version_name, "default_avatar.jpg" ].compact.join("_")
+  end
+end
