e86f2c740bf09d3ce995f37351212d260771763d
Ilya <simonewarlet@gmail.com>
2024-12-11 14:48:33 +0200

    Add resizing for medium version to half of image, delete comments

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,36 +1,26 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
 
   def extension_allowlist
     %w[jpg jpeg png]
   end
 
-  version :full do
-    process resize_to_fill: [ 1920, 1080 ]
+  def resize_to_half
+    manipulate! do |img|
+      new_width = img[:width] / 2
+      new_height = img[:height] / 2
+      img.resize "#{new_width}x#{new_height}"
+      img
+    end
   end
 
   version :medium do
-    process resize_to_fill: [ 300, 300 ]
+    process :resize_to_half
   end
 
   version :thumbnail do
