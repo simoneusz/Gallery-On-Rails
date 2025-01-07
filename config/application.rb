dfdca53f5e9298885b44457619a99e385fa8b481
Ilya <simonewarlet@gmail.com>
2025-01-07 15:16:35 +0200

    Add custom modal for category deletion

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module GalleryOnRails
   class Application < Rails::Application
-    config.assets.paths << Rails.root.join("app", "assets", "stylesheets")
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
