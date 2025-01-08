7c1a4bdee8510edb0bd0adeda4f8f231ac734279
Ilya <simonewarlet@gmail.com>
2025-01-08 10:34:19 +0200

    Comment channels

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,15 +3,4 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 import * as bootstrap from "bootstrap"
-import "./channels"
-
-import subscribeToCategory from "./category_subscription_channel";
-
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("eawe")
-    const categoryId = document.getElementById("category").dataset.categoryId;
-
-    subscribeToCategory(categoryId, (data) => {
-        alert(data.message);
-    });
-});
+// import "./channels"
