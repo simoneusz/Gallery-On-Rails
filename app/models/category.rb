c920f575c8e9ed91be0167a177bdca4577f8673d
Ilya <simonewarlet@gmail.com>
2025-01-09 12:23:01 +0200

    Add arrows to navigate caregories

--- app/models/category.rb
+++ app/models/category.rb
@@ -14,4 +14,18 @@ class Category < ApplicationRecord
   def total_likes_count
     images.sum(:likes_count)
   end
+
+  def next
+    category = Category.where("id > ?", self.id).first
+    category.nil? ? Category.first : category
+  end
+
+  def previous
+    category = Category.where("id < ?", self.id).first
+    category.nil? ? Category.first : category
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "created_at", "description", "id", "id_value", "slug", "title", "updated_at", "user_id" ]
+  end
 end
