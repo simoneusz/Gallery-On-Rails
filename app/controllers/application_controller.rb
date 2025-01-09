b0c0c921114be0b98ee7d7c4423d4605d2264352
Ilya <simonewarlet@gmail.com>
2025-01-09 13:58:27 +0200

    Add logging for user navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,23 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :track_navigation
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [ :avatar ])
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :avatar ])
+  end
+
+  private
+
+  def track_navigation
+    return unless current_user
+    ActivityLog.create(
+      user: current_user,
+      action_type: "navigation",
+      url: request.fullpath
+    )
   end
 end
