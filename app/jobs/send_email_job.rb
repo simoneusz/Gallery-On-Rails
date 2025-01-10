cecb15e0572204af4eeb48d84e33d9f297c3f99d
Ilya <simonewarlet@gmail.com>
2025-01-10 16:59:06 +0200

    Fix send email job empty code

--- app/jobs/send_email_job.rb
+++ app/jobs/send_email_job.rb
@@ -2,5 +2,11 @@ class SendEmailJob < ApplicationJob
   queue_as :default
 
   def perform(user_id, subject, body)
+    user = User.find(user_id)
+    return if user.nil? || subject.nil? || body.nil?
+    puts "At #{Time.now}: "
+    puts "Starting to sending email for #{user.username}.."
+    puts user.email
+    UserMailer.custom_mail(user).deliver_later
   end
 end
