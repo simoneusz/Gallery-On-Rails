66dfd282bfab204378775062d8e3a3c461ba6e45
Ilya <simonewarlet@gmail.com>
2025-01-10 13:16:46 +0200

    Add resque with cleanup task

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module GalleryOnRails
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
+    config.active_job.queue_adapter = :resque
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
