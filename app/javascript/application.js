774591c3d3a0d5ebb8ab20f0c54c3fe77a1e3879
Ilya <simonewarlet@gmail.com>
2025-01-13 10:11:32 +0200

    Remove masonry js from views and use importmap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
 
-import * as bootstrap from "bootstrap"
+import * as bootstrap from "bootstrap";
+import "masonry-layout";
 // import "./channels"
+
+document.addEventListener("turbo:load", function () {
+  let masonryContainer = document.querySelector("#masonry-gallery");
+  if (masonryContainer) {
+    new window.Masonry(masonryContainer, {
+      itemSelector: ".masonry",
+      percentPosition: true,
+      gutter: 0,
+    });
+  }
+});
