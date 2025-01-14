f038bcd909c6d7a28a175fd82ca60a697a95b4d8
Ilya <simonewarlet@gmail.com>
2025-01-14 13:51:58 +0200

    Remove image from permited parags

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -50,6 +50,6 @@ class CategoriesController < ApplicationController
   end
 
   def category_params
-    params.require(:category).permit(:title, :description, :image)
+    params.require(:category).permit(:title, :description)
   end
 end
