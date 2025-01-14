d7d9be43abba986f17c90a7551206ca5cfd8545d
Ilya <simonewarlet@gmail.com>
2025-01-14 16:49:43 +0200

    Fix redirect path

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe " redirecded from GET /profile" do
+  describe " redirecded from GET /users/profile" do
     it "works! (now write some real specs)" do
-      get profile_path
+      get edit_user_registration_path
       expect(response).to have_http_status(302)
     end
   end
