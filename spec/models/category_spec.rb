b9f88e274937b93358a21bf0668fb892720ea7b9
Ilya <simonewarlet@gmail.com>
2025-01-15 14:24:56 +0200

    Change shoulda to is_expected.to

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   let(:user) { create(:user) }
-  subject { build(:category, user: user) }
+  subject { build(:category, user:) }
 
   describe 'validations' do
-    it { should validate_presence_of(:title) }
-    it { should validate_length_of(:title).is_at_least(1).is_at_most(24) }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_length_of(:title).is_at_least(1).is_at_most(24) }
   end
 
   describe 'associations' do
-    it { should belong_to(:user) }
-    it { should have_many(:images).order(likes_count: :desc).dependent(:destroy) }
-    it { should have_many(:comments).dependent(:destroy) }
-    it { should have_many(:subscriptions).dependent(:destroy) }
-    it { should have_many(:subscribers).through(:subscriptions).source(:user) }
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to have_many(:images).order(likes_count: :desc).dependent(:destroy) }
+    it { is_expected.to have_many(:comments).dependent(:destroy) }
+    it { is_expected.to have_many(:subscriptions).dependent(:destroy) }
+    it { is_expected.to have_many(:subscribers).through(:subscriptions).source(:user) }
   end
 
   describe 'methods' do
@@ -27,14 +27,17 @@ RSpec.describe Category, type: :model do
     end
 
     describe '#total_likes_count' do
+      subject { category.total_likes_count }
+
       it 'returns the total likes count for all images in the category' do
-        expect(category.total_likes_count).to eq(8)
+        is_expected.to eq(8)
       end
     end
 
     describe '#next' do
+      before { other_category.destroy }
+
       it 'returns the first category if no next category exists' do
-        other_category.destroy
         expect(category.next).to eq(Category.first)
       end
     end
