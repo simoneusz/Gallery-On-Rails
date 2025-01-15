742bd0471faf1804d71bc1f3baa32886fb566adc
Ilya <simonewarlet@gmail.com>
2025-01-15 11:01:16 +0200

    Add settign up a locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_notifications
+  before_action :set_locale
   after_action :track_navigation
 
   protected
@@ -12,7 +13,15 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :avatar ])
   end
 
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 
   def track_navigation
     return unless current_user
