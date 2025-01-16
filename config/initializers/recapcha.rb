bff0ba1e5ec316c0cda403e9d8049de026da3161
Ilya <simonewarlet@gmail.com>
2025-01-16 13:40:01 +0200

    Fix keys

--- config/initializers/recapcha.rb
+++ config/initializers/recapcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key = ENV['recaptcha_secret_key'] || Rails.application.credentials.recaptcha_site_key
-  config.secret_key = ENV['recaptcha_site_key'] || Rails.application.credentials.recaptcha_secret_key
+  config.site_key = ENV['recaptcha_site_key'] || Rails.application.credentials.recaptcha_site_key
+  config.secret_key = ENV['recaptcha_secret_key'] || Rails.application.credentials.recaptcha_secret_key
 end
